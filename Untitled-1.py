c0510c32ab6d9d0d4d5750d736d173829a0d23ca
jiwhan1123 <79753445+jiwhan1123@users.noreply.github.com>
2024-01-17 16:07:38 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras import layers
+import numpy as np
+import tensorflow as tf
